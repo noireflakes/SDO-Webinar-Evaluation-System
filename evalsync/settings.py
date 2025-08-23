41631028d491c08a8f6c9f1adb676721b53f647d
john philip baylon <johnphilipbaylon3@gmail.com>
2025-08-23 17:03:55 +0800

    added origins

--- evalsync/settings.py
+++ evalsync/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com','127.0.0.1', 'localhost',".up.railway.app"]
 
-CSRF_TRUSTED_ORIGIN=[
+CSRF_TRUSTED_ORIGINS=[
     'https://*.onrender.com','127.0.0.1', 'localhost',"https://*.up.railway.app","https://sdo-webinar-evaluation-system-production.up.railway.app"
 ]
 
