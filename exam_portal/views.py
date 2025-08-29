d35eb77061f95d2cb6b5ea83c61658a41c15655c
john philip baylon <johnphilipbaylon3@gmail.com>
2025-08-29 21:00:15 +0800

    fix test url

--- exam_portal/views.py
+++ exam_portal/views.py
@@ -228,7 +228,7 @@ def qr_evalution(request, id):
     type=webinar.event_type
     qr=EvalQR.objects.filter(test__id=id, type=type)
     
-    url=f"https://sdo-webinar-evaluation-system-production.up.railway.app/webinar/questionaire/{webinar.id}/"
+    url=f"https://sdo-webinar-evaluation-system-production.up.railway.app/exam_portal/questionaire/{webinar.id}/"
 
     if not qr:
         
