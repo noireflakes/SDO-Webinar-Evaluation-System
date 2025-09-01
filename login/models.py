0f05c5e11ebba1c91304de73045c244558739861
john philip baylon <johnphilipbaylon3@gmail.com>
2025-09-01 17:19:29 +0800

    fix db otp issue

--- login/models.py
+++ login/models.py
@@ -21,13 +21,16 @@ class TrustedDevice(models.Model):
     def __str__(self):
         return f"{self.user.first_name}_{self.id}"
     
-
+    
 class Otp(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="otp_code")
-    otp=models.CharField(max_length=400)
-    created_at = models.DateTimeField(default=timezone.now) 
-    retry=models.IntegerField(default=0)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    secret_key=models.CharField(max_length=20)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    retry = models.IntegerField(default=0)
+    last_attempt = models.DateTimeField(null=True, blank=True)  
     def __str__(self):
         return f"{self.id}-{self.user}-{self.otp}"
 
+Otp
